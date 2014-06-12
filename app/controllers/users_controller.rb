56e31ea09a9c975006a7ccb401df765cca2f3d3a
alexshook <alexandra.shook@gmail.com>
2014-06-12 00:04:50 -0400

    work on user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+
+  respond_to :html, :json
+
   def index
   end
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save!
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+  params.require(:user).permit(:email, :name, :password, :password_confimation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  :has_secure_password
+
+  validates :email, :name presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confimation, presence: { if: :password }
+
 end
