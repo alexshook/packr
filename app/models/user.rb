37fb36258246606b24e1f8e817908450aead4eb9
alexshook <alexandra.shook@gmail.com>
2014-06-20 17:36:24 -0400

    form for user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   :has_secure_password
 
-  validates :email, :name presence: true
+  validates :email, :name, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true, confirmation: true
   validates :password_confimation, presence: { if: :password }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  match 'signup', to: 'users#new', via: :get
+
   resources :users
 end
